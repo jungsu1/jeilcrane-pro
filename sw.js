cdb4fc6ff9ce4171ad8e713a8fcd06567cc8e808
jungsu1 <yjs10000@naver.com>
2026-09-22 13:33:43 +0900

    Refresh PWA cache for statement fix

--- sw.js
+++ sw.js
@@ -16,7 +16,8 @@ self.addEventListener("install", (event) => {
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
-      .then(() => self.clients.claim())
+      .then(() => self.clients.claim())1
+const CACHE_NAME = "jeilcrane-pro-cache-v3";
   );
 });
 
