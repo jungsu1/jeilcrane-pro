63367a375973d402c919e53ba5aca2786a04021a
jungsu1 <yjs10000@naver.com>
2026-09-22 11:59:56 +0900

    Fix direct collection exclusion in statements

--- app.js
+++ app.js
@@ -1994,7 +1994,8 @@ function buildSettlementTrendBarHeight(monthSales, maxSales) {
     return "0px";
   }
 
-  if (monthSales <= 0) {
+  if (monthSales <= 0) {2241
+equipmentJobs: filterBySite(filterSettlementStatementJobs(statementBaseReport.equipmentJobs || [], includeDirectCollection)),
     return "4px";
   }
 
